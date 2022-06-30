adb041568c2f5f00407f56aa0b7575d16994f775
Nikitamt <ntarasov0326@icloud.com>
2022-06-30 15:07:43 +0500

    Initial commit

---
+++ src/main/java/com/nikita/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.nikita.spring.mvc_hibernate_aop.service;
+
+import com.nikita.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+}
